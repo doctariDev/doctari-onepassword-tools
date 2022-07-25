5f99cd7e25f0afe2fc01dffad64001be0613a61d
Valeriu Chis <valeriu.chis@doctarigroup.com>
2022-07-25 15:02:50 +0300

    feat(parser): look for templates in specified root folder

---
+++ bin/op-load-env.js
@@ -0,0 +1,128 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const child_process = require("child_process");
+const path = require("path");
+
+const SKIPPED_DIRS = ['node_modules'];
+const TEMPLATE_NAME = 'env.template.json'
+
+function op(args, input) {
+    const config = input
+        ? { stdio: 'pipe', input: Buffer.from(input) }
+        : { stdio: ['inherit', 'pipe', 'pipe'] };
+
+    const {status, error, stdout, stderr} = child_process.spawnSync( "op", args, config);
+
+    if (error) {
+        throw error;
+    }
+
+    if (stderr && stderr.length) {
+        throw new Error(stderr.toString('utf-8'));
+    }
+
+    if (status !== 0) {
+        throw new Error(`command exited with status ${status}`);
+    }
+
+    return stdout.toString('utf-8');
+}
+
+function envSubstitution(input) {
+    for (const [key, value] of Object.entries(process.env)) {
+        const regex = new RegExp(`\\$${key}`, 'g');
+        input = input.replace(regex, value);
+    }
+
+    return input;
+}
+
+function findTemplates(folder) {
+    try {
+        const results = [];
+        const children = fs.readdirSync(folder, {withFileTypes: true})
+        for (const child of children) {
+            if (SKIPPED_DIRS.includes(child.name)) {
+                continue;
+            }
+            if (child.isDirectory()) {
+                results.push(...findTemplates(path.join(folder, child.name)));
+            }
+            if (child.name === TEMPLATE_NAME) {
+                results.push(path.normalize(path.join(folder, child.name)));
+            }
+        }
+        return results;
+    }
+    catch (e) {
+        console.error(e.message);
+        return [];
+    }
+}
+
+async function loadEnvironment(folder, environment, token) {
+    const templates = findTemplates(folder);
+
+    if (!templates.length) {
+        console.warn(`No templates found in folder ${folder}, make sure to name them env.template.json`);
+        return;
+    }
+
+    for (const template of templates) {
+        const env = await processTemplate(template, environment, token)
+        const outputPath = path.join(
+            path.dirname(template),
+            '.env',
+        );
+        fs.writeFileSync(outputPath, env, { encoding: 'utf-8' });
+    }
+}
+
+async function processTemplate(inputPath, env, token) {
+    console.log(`[info] Processing template at ${inputPath} (${env})`);
+    const template = JSON.parse(fs.readFileSync(inputPath, {encoding: 'utf8'}));
+    if (Object.keys(template).length < 1) {
+        console.warn(`[warn] Empty template at ${inputPath}, skipping`);
+        return '';
+    }
+    const processedLines = [];
+    for (const [key, value] of Object.entries(template)) {
+        if (typeof value === 'string') {
+            processedLines.push(`${key}=${envSubstitution(value)}`);
+            continue;
+        }
+        if (!value[env]) {
+            throw new Error('Missing value for ${key}[${env}], please fix the template');
+        }
+        processedLines.push(`${key}=${value[env]}`);
+    }
+    return op(['inject', '--session', token], processedLines.join('\n'));
+}
+
+async function main() {
+    const env = process.env.STAGE;
+    if (!env) {
+        throw new Error('no stage set, use export STAGE=<stage>');
+    }
+
+    const [folder] = process.argv.slice(2);
+    if (!folder) {
+        throw new Error('Usage: load-secrets <path-to-template>');
+    }
+
+    console.log(`[info] 1password-cli version: ${op(['--version'])}`);
+    const token = process.env.OP_SESSION_TOKEN || op(['signin', '--raw']);
+
+    await loadEnvironment(folder, env, token);
+}
+
+main().then(
+    () => {
+        console.error('Done')
+    },
+    (e) => {
+        console.error(e.message);
+        process.exit(1);
+    }
+);
